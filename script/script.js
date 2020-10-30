c26b48a828368b887e1a398a2a35f02b388d57f9
Marcos Vinicius Gasparoto Bauab <marcosbauab@gmail.com>
2020-10-30 19:48:42 -0300

    Começo da lógica

--- script/script.js
+++ script/script.js
@@ -1,4 +1,30 @@
-function funcao(){
-    var a = document.getElementById("Titulo") 
-    a.style.backgroundColor = "red"
-    }
+var p = document.querySelector(".resultado")
+
+
+
+
+
+/*function teclaPressionada(e) {
+    //p.innerHTML += `${e.code}`
+    for(var i = 0; i < 10; i++){
+        if(e.code == `Numpad${i}`){
+            p.innerHTML += `${i}`
+        }
+        if(e.code == `Digit${i}`){
+            p.innerHTML += `${i}`
+        }
+    } 
+    switch (e.code) {
+        case "NumpadMultiply":
+            p.innerHTML += `*`
+            break;
+        case "Backspace":
+            p.innerHTML -= `*`
+            break;
+        
+        
+        default:
+            break;
+    }  
+    
+}*/
